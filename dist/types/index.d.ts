62bc2ccfde00fcf6fc19e58440f6c2df467bf560
zhangsanshi <zhangsanshi5566@gmail.com>
2020-05-14 22:26:47 +0800

    rewrite plugin

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,6 +1,7 @@
 import babel from './babel';
 import wrap from './wrap';
 import plugin from './plugin';
+import newPlugin from './newPlugin';
 import global from './global.babel';
 import { default as helper } from './micro';
-export { babel, plugin, helper, wrap, global, };
+export { babel, plugin, helper, wrap, global, newPlugin, };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import babel from './babel';
 import wrap from './wrap';
 import plugin from './plugin';
+import newPlugin from './newPlugin';
 import global from './global.babel';
 import { default as helper } from './micro';
 export {
@@ -9,4 +10,5 @@ export {
     helper,
     wrap,
     global,
+    newPlugin,
 };
